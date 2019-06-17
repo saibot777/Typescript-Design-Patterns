ef1e979551f3bcdde91f81b098a735ad024e696b
saibot777 <stefantrajkovic777@gmail.com>
2019-06-17 12:01:30 +0200

    add company marker method

--- 2018-2019/pattern-app/src/CustomMap.ts
+++ 2018-2019/pattern-app/src/CustomMap.ts
@@ -1,4 +1,5 @@
 import { User } from "./User";
+import { Company } from "./Company";
 
 export class CustomMap {
     private googleMap: google.maps.Map;
@@ -19,6 +20,16 @@ export class CustomMap {
             position: {
                 lat: user.location.lat,
                 lng: user.location.lng
+            }
+        });
+    }
+
+    public addCompanyMarker(company: Company): void {
+        new google.maps.Marker({
+            map: this.googleMap,
+            position: {
+                lat: company.location.lat,
+                lng: company.location.lng
             }
         });
     }
