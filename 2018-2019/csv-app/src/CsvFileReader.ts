92fa90a5c9fa8782e20c03188d96d64c14c2171b
saibot777 <stefantrajkovic777@gmail.com>
2019-07-04 13:40:32 +0200

    abstracting csv phase one

--- 2018-2019/csv-app/src/CsvFileReader.ts
+++ 2018-2019/csv-app/src/CsvFileReader.ts
@@ -4,10 +4,12 @@ import { MatchResult } from './Matchresult';
 
 type MatchData = [Date, string, string, number, number, MatchResult, string];
 
-export class CsvFileReader {
-    data : MatchData[] = []
-
+export abstract class CsvFileReader {
+    data : MatchData[] = [];
+    
     constructor(public filename: string) {}
+
+    abstract mapRow(row: string[]): MatchData;
 
     read(): void {
         this.data = fs
@@ -19,17 +21,5 @@ export class CsvFileReader {
             return row.split(',');
         })
         .map(this.mapRow);
-    }
-
-    mapRow(row: string[]): MatchData {
-        return [
-            dateStringToDate(row[0]),
-            row[1],
-            row[2],
-            parseInt(row[3]),
-            parseInt(row[4]),
-            row[5] as MatchResult,
-            row[6]
-        ];
     }
 }
